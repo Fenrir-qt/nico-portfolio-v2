d202b8c1357f47db323b88179e0058008e6bb6f9
Fenrir-qt <mickburog87@gmail.com>
2025-09-04 06:26:39 +0800

    Remove section tag on footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,11 @@
 export default function Footer() {
   return (
     <>
-      <section>
         <footer>
           <div className="w-full text-center p-5 text-sm dark:text-white">
             &copy; 2025 | Nico Angelo P. Burog | All Rights Reserved.
           </div>
         </footer>
-      </section>
     </>
   );
 }
